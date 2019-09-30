f04de874cfadb5b53eee6d29c1ae2475bf72ccea
pocketdru <pocketdru@gmail.com>
2019-09-30 17:26:12 -0600

    Meals Controller created and added to routes

--- app/Http/Controllers/MealsController.php
+++ app/Http/Controllers/MealsController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
-class UserController extends Controller
+class MealsController extends Controller
 {
     /**
      * Display a listing of the resource.
--- database/migrations/2019_09_30_231909_create_meals_table.php
+++ database/migrations/2019_09_30_231909_create_meals_table.php
@@ -14,6 +14,7 @@ class CreateMealsTable extends Migration
     {
         Schema::create('meals', function (Blueprint $table) {
             $table->increments('id');
+            $table->string("name");
             $table->timestamps();
         });
     }
