b2f29784e041c6254560e390bef323139a4e97ef
pocketdru <pocketdru@gmail.com>
2019-10-01 23:16:24 -0600

    html background image added and home.blade.php content added

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,5 +1,4 @@
-
- @extends("layout")
+@extends("layout")
 
 
 @section("title")
@@ -7,5 +6,12 @@ My Fitness buddy
 @stop
 
 @section("content")
-    Welcome User!
+    Welcome, Stranger!
+    <div class="row">
+        <div class=col-12>
+            <h6>
+                <a href="login">Login </a> or <a href="/register">register</a> to get started
+            </h6>  
+        </div>
+    </div> 
 @stop
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" type="text/css">
+        <link rel="stylesheet" href="{{ URL::asset('assets/css/app.css') }}" type="text/css">
 
     </head>
     <body>
